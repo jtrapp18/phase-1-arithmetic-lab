b87346a8da6695fc36de25b511b2da32e4cd0608
+ <jacqueline.trapp18@gmail.com>
2024-10-28 17:36:44 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+
+const multiply = num1*num2
+
+const random = Math.floor(Math.random() * 10)
+
+const num3 = 5
+const num4 = 1
+
+const mod = num3 - num4
+
+const num5 = 20
+
+let max;
+num5>num3 ? max=num5 : max=num3
